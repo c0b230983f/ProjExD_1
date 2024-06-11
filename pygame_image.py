22da9f237141dc886efc7ee21500621f160b58ac
SSR_GODfushimity <C0B230983f@edu.teu.ac.jp>
2024-06-11 16:34:51 +0900

    間延び対策２

--- pygame_image.py
+++ pygame_image.py
@@ -10,16 +10,21 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img2, True, False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%800
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2,[-x+1600,0])
+        screen.blit(bg_img,[-x+3200,0])
+        screen.blit(bg_img2,[-x+4800,0])
         kk_rct = kk_img.get_rect() #高価トンrectの抽出
         kk_rct.center = 300,200
         screen.blit(kk_img,kk_rct) #kk_imgをkk_rctの設定に従い貼り付け
